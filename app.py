dc121cf546a316d12254b2104fce01383653dc35
divyam29 <divyamjain29@gmail.com>
2022-03-13 08:27:55 +0530

    Updated JS

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-from flask import Flask, render_template
-from logging import FileHandler,WARNING
+from flask import Flask, render_template,request, session, redirect
+from logging import FileHandler, WARNING
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+local_server = True
+
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/hackowasp"
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/hackowasp"
+
+db = SQLAlchemy(app)
+
+class contact(db.Model):
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
 
 @app.route("/")
 def home():
     return render_template('Home.html')
 
+
 @app.route("/About")
 def about():
     return render_template('About.html')
+
 
 @app.route("/Our-App")
 def ourapp():
     return render_template('Our-App.html')
 
+
 @app.route("/Contact")
 def contact():
     return render_template('Contact-US.html')
 
+
 @app.route("/SignIn")
 def signin():
     return render_template('Sign-In.html')
-
-
 
 
 if __name__ == "main":
